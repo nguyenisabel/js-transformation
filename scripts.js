5fc9b12c8432e7227f507f4dde93cd6b7bf2fbbf
nguyenisabel <122078261+nguyenisabel@users.noreply.github.com>
2023-01-27 03:46:08 -0800

    adjust sizing

--- scripts.js
+++ scripts.js
@@ -11,11 +11,3 @@ $(document).ready(function() {
         $("#p3").show();
     });
 });
-
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
-
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
